8fb913a9c306d6d37a85c635fba268e1151272f6
Thunder544 <vargman54@gmail.com>
2017-02-09 15:37:40 -0600

    Create Markov.py

---
+++ Markov.py
@@ -0,0 +1,29 @@
+import random
+def markov():
+    startIndex = random.randint(0,len(text))
+    currentGram = text[startIndex:startIndex+order]
+
+    result = currentGram
+    for i in range(200-order):
+        possible = ngrams[currentGram]
+
+        n = random.choice(possible)
+        result += n
+        currentGram = result[len(result)-order:len(result)]
+    return result
+
+text = ""
+
+order = 5
+ngrams = {}
+
+
+for c in range(len(text)-(order-1)):
+    gram = text[c:c+order]
+
+    if not gram in ngrams:
+        ngrams[gram] = []
+
+    ngrams[gram].append(text[c+order:c+order+1])
+
+print(markov())
